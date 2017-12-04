c7c65a042653c51472a52565d81293e9a48ad2a0
Arnout Engelen <arnouten@bzzt.net>
2017-12-04 18:20:44 +0100

    With version updates
    Not sure about copyFromPorts but seems to work for me

--- project/Versions.scala
+++ project/Versions.scala
@@ -4,17 +4,17 @@
 object Versions {
   val Scala             = "2.11.11"
   val JDK               = "1.8"
-  /*
     val reactiveKafkaVersion = "0.18"
     val akkaVersion = "2.5.6"
     val akkaHttpVersion = "10.0.10"
     val akkaHttpJsonVersion = "1.18.1"
-  */
 
+  /*
   val reactiveKafkaVersion = "0.16"
   val akkaVersion = "2.4.19"
   val akkaHttpVersion = "10.0.9"
   val akkaHttpJsonVersion = "1.17.0"
+  */
 
   val Curator        = "4.0.0"
 
