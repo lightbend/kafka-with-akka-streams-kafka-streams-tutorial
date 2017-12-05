c5d4e2b4d82c0e6d76c0b4deb94c97d92e82fd24
blublinsky <boris.lublinsky@lightbend.com>
2017-12-05 14:28:11 -0600

    Added file persistence

--- akkaserverpersistent/src/main/scala/com/lightbend/modelserver/actor/actors/ModelServingActor.scala
+++ akkaserverpersistent/src/main/scala/com/lightbend/modelserver/actor/actors/ModelServingActor.scala
@@ -3,6 +3,7 @@ package com.lightbend.modelserver.actor.actors
 import akka.actor.{Actor, Props}
 import com.lightbend.model.winerecord.WineRecord
 import com.lightbend.modelServer.model.{Model, ModelToServeStats, ModelWithDescriptor}
+import com.lightbend.modelserver.actor.persistence.FilePersistence
 
 // Workhorse - doing model serving for a given data type
 
@@ -11,14 +12,22 @@ class ModelServingActor(dataType : String) extends Actor {
   println(s"Creating model serving actor $dataType")
   private var currentModel: Option[Model] = None
   private var newModel: Option[Model] = None
-  var currentState: Option[ModelToServeStats] = None // exposed in materialized value
+  var currentState: Option[ModelToServeStats] = None
   private var newState: Option[ModelToServeStats] = None
+
+  override def preStart {
+    println("Restoring")
+    val state = FilePersistence.restoreState(dataType)
+    newState = state._2
+    newModel = state._1
+  }
 
   override def receive = {
     case model : ModelWithDescriptor => {
       // Update model
       newState = Some(new ModelToServeStats(model.descriptor))
       newModel = Some(model.model)
+      FilePersistence.saveState(dataType, newModel.get, newState.get)
       sender() ! "Done"
     }
     case record : WineRecord => {
---
+++ akkaserverpersistent/src/main/scala/com/lightbend/modelserver/actor/persistence/FilePersistence.scala
@@ -0,0 +1,117 @@
+package com.lightbend.modelserver.actor.persistence
+
+import java.io._
+
+import com.lightbend.model.modeldescriptor.ModelDescriptor
+import com.lightbend.modelServer.model.{Model, ModelToServeStats}
+import com.lightbend.modelServer.model.PMML.PMMLModel
+import com.lightbend.modelServer.model.tensorflow.TensorFlowModel
+
+object FilePersistence {
+
+  private final val basDir = "persistence/"
+
+  private val factories = Map(
+    ModelDescriptor.ModelType.PMML.index -> PMMLModel,
+    ModelDescriptor.ModelType.TENSORFLOW.index -> TensorFlowModel
+  )
+
+  def restoreState(dataType: String) : (Option[Model], Option[ModelToServeStats]) = {
+    getDataInputStream(dataType) match {
+      case Some(input) => (readModel(input), readServingInfo(input))
+      case _ => (None, None)
+    }
+   }
+
+  private def getDataInputStream(fileName: String) : Option[DataInputStream] = {
+    val file = new File(basDir + fileName)
+    file.exists() match {
+      case true => Some(new DataInputStream(new FileInputStream(file)))
+      case _ => None
+    }
+  }
+
+  private def readModel(input : DataInputStream) : Option[Model] = {
+    input.readLong.toInt match{
+      case length if length > 0 => {
+        val `type` = input.readLong.toInt
+        val bytes = new Array[Byte](length)
+        input.read(bytes)
+        factories.get(`type`) match{
+          case Some(factory) => try {
+            Some(factory.restore(bytes))
+          }
+          catch {
+            case t: Throwable =>
+              System.out.println("Error Deserializing model")
+              t.printStackTrace()
+              None
+          }
+          case _ => None
+        }
+      }
+      case _ => None
+    }
+  }
+
+  private def readServingInfo(input: DataInputStream) : Option[ModelToServeStats] = {
+    input.readLong match {
+      case length if length > 0 => {
+        try {
+          Some(ModelToServeStats(input.readUTF, input.readUTF, input.readLong, input.readLong, input.readDouble, input.readLong, input.readLong))
+        } catch {
+          case t: Throwable =>
+            System.out.println("Error Deserializing serving info")
+            t.printStackTrace()
+            None
+        }
+      }
+      case _ => None
+    }
+  }
+
+
+  def saveState(dataType: String, model: Model, servingInfo: ModelToServeStats) : Unit = {
+    println("Saving state")
+    val output = getDataOutputStream(dataType)
+    writeModel(output, model)
+    writeServingInfo(output, servingInfo)
+  }
+
+  private def getDataOutputStream(fileName: String) : DataOutputStream = {
+    val file = new File(basDir + fileName)
+    if(!file.exists())
+      file.createNewFile()
+    new DataOutputStream(new FileOutputStream(file))
+  }
+
+  private def writeModel(output : DataOutputStream, model: Model) : Unit = {
+    try {
+      val bytes = model.toBytes()
+      output.writeLong(bytes.length)
+      output.writeLong(model.getType)
+      output.write(bytes)
+    } catch {
+      case t: Throwable =>
+        System.out.println("Error Serializing model")
+        t.printStackTrace()
+    }
+  }
+
+  private def writeServingInfo(output: DataOutputStream, servingInfo: ModelToServeStats ): Unit = {
+    try{
+      output.writeLong(5)
+      output.writeUTF(servingInfo.description)
+      output.writeUTF(servingInfo.name)
+      output.writeLong(servingInfo.since)
+      output.writeLong(servingInfo.usage)
+      output.writeDouble(servingInfo.duration)
+      output.writeLong(servingInfo.min)
+      output.writeLong(servingInfo.max)
+    }catch {
+      case t: Throwable =>
+        System.out.println("Error Serializing servingInfo")
+        t.printStackTrace()
+    }
+  }
+}
