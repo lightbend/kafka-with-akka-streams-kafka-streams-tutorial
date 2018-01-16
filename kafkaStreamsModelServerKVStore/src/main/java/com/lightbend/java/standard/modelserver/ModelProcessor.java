35179badeff7ec901abeee72757a0ee9aa7d3ba0
Dean Wampler <dean@concurrentthought.com>
2018-01-15 20:07:07 -0600

    More project renaming and consistent use of java and scala package names

---
+++ kafkaStreamsModelServerKVStore/src/main/java/com/lightbend/java/standard/modelserver/ModelProcessor.java
@@ -0,0 +1,39 @@
+package com.lightbend.java.standard.modelserver;
+
+import com.lightbend.configuration.kafka.ApplicationKafkaParameters;
+import com.lightbend.model.ModelWithDescriptor;
+import com.lightbend.model.ModelServingInfo;
+import com.lightbend.standard.modelserver.store.StoreState;
+import org.apache.kafka.streams.processor.AbstractProcessor;
+import org.apache.kafka.streams.processor.ProcessorContext;
+import org.apache.kafka.streams.state.KeyValueStore;
+
+import java.util.Objects;
+import java.util.Optional;
+
+/**
+ * Created by boris on 7/12/17.
+ */
+public class ModelProcessor extends AbstractProcessor<byte[], Optional<ModelWithDescriptor>> {
+
+    private KeyValueStore<Integer, StoreState> modelStore;
+
+    @Override
+    public void process(byte[] key, Optional<ModelWithDescriptor> modelWithDescriptor) {
+
+        StoreState state = modelStore.get(ApplicationKafkaParameters.STORE_ID);
+        if(state == null)
+            state = new StoreState();
+        state.setNewModel(modelWithDescriptor.get().getModel());
+        state.setNewServingInfo(new ModelServingInfo(modelWithDescriptor.get().getDescriptor().getName(),
+                    modelWithDescriptor.get().getDescriptor().getDescription(), 0));
+        modelStore.put(ApplicationKafkaParameters.STORE_ID, state);
+        return;
+    }
+
+    @Override
+    public void init(ProcessorContext context) {
+        modelStore = (KeyValueStore<Integer, StoreState>) context.getStateStore(ApplicationKafkaParameters.STORE_NAME);
+        Objects.requireNonNull(modelStore, "State store can't be null");
+    }
+}
---
+++ kafkaStreamsModelServerKVStore/src/main/scala/com/lightbend/scala/standard/modelserver/store/StoreState.scala
@@ -0,0 +1,15 @@
+package com.lightbend.scala.standard.modelserver.scala.store
+
+import com.lightbend.modelServer.model._
+
+case class StoreState(var currentModel: Option[Model] = None, var newModel: Option[Model] = None,
+                      var currentState: Option[ModelToServeStats] = None, var newState: Option[ModelToServeStats] = None){}
+
+object StoreState{
+  val instance = new StoreState()
+  def apply(): StoreState = instance
+}
+
+case class HostStoreInfo (host: String, port: Int, storeNames: Seq[String]){
+  override def toString: String = s"HostStoreInfo{host= + $host + port=$port storeNames= ${storeNames.mkString(",")}}"
+}
