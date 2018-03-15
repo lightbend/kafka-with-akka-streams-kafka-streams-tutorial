927a5abc135c3015fa844590204a0fae0a8011af
Dean Wampler <dean@concurrentthought.com>
2018-03-14 19:28:16 -0500

    Added command-line options to pick the stream builder, requiring a slight refactoring to how the builder is chosen.

--- kafkaStreamsModelServer/src/main/scala/com/lightbend/scala/kafkastreams/modelserver/KafkaModelServer.scala
+++ kafkaStreamsModelServer/src/main/scala/com/lightbend/scala/kafkastreams/modelserver/KafkaModelServer.scala
@@ -9,14 +9,33 @@ import com.lightbend.scala.kafkastreams.modelserver.memorystore.MemoryStoreStrea
 import com.lightbend.scala.kafkastreams.modelserver.standardstore.StandardStoreStreamBuilder
 import com.lightbend.scala.kafkastreams.queriablestate.inmemory.RestServiceInMemory
 import org.apache.kafka.common.serialization.Serdes
-import org.apache.kafka.streams.StreamsConfig
+import org.apache.kafka.streams.{KafkaStreams, StreamsConfig}
+
+import scala.util.control.NonFatal
 
 object KafkaModelServer {
 
   private val port = 8888 // Port for queryable state
 
 
+  def help(message: String = "", exitCode: Int = 0): Nothing = {
+    println(
+      s"""
+         |$message
+         |
+         |KafkaModelServer -h | --help | m | memory | c | custom | s | standard
+         |
+         |-h | --help   Print this help and exit
+         | m | memory   Use the in-memory state store ("naive") implementation
+         | c | custom   Use the custom state store implementation (default)
+         | s | standard Use the built-in ("standard") state store implementation
+         |
+    """.stripMargin)
+    sys.exit(exitCode)
+  }
+
   def main(args: Array[String]): Unit = {
+
     System.out.println("Using kafka brokers at " + KAFKA_BROKER)
     val streamsConfiguration = new Properties
     // Give the Streams application a unique name.  The name must be unique in the Kafka cluster
@@ -31,27 +50,24 @@ object KafkaModelServer {
     // Default serdes
     streamsConfiguration.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.ByteArray.getClass)
     streamsConfiguration.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.ByteArray.getClass)
-/*
-    // memory Store
-    // Create topology
-    val streams = MemoryStoreStreamBuilder.createStreams(streamsConfiguration)
-    // Start the Restful proxy for servicing remote access to state stores
-    RestServiceInMemory.startRestProxy(streams, port)
-*/
 
-    // Custom Store
-    // Create topology
-    val streams = CustomStoreStreamBuilder.createStreamsFluent(streamsConfiguration)
-    // Start the Restful proxy for servicing remote access to state stores
-    RestServiceStore.startRestProxy(streams, port, "custom")
+    // You can either pick which one to run using a command-line argument, or for ease of use
+    // with the IDE Run menu command, just switch which line is commented out for "case Nil => ...".
+    val streams: KafkaStreams = args.toSeq match {
+      case ("m"  | "memory")   +: tail =>
+        setupMemoryStoreStreams(streamsConfiguration)
+      case ("c"  | "custom")   +: tail =>
+        setupCustomStoreStreams(streamsConfiguration)
+      case ("s"  | "standard") +: tail =>
+        setupStandardStoreStreams(streamsConfiguration)
+      case ("-h" | "--help")   +: tail => help()
+      case Nil =>
+//        setupMemoryStoreStreams(streamsConfiguration)
+        setupCustomStoreStreams(streamsConfiguration)
+//        setupStandardStoreStreams(streamsConfiguration)
+      case _ => help(s"Unexpected arguments: ${args.mkString(" ")}", 1)
+    }
 
-/*
-    // Standard Store
-    // Create topology
-    val streams = StandardStoreStreamBuilder.createStreamsFluent(streamsConfiguration)
-    // Start the Restful proxy for servicing remote access to state stores
-    RestServiceStore.startRestProxy(streams, port, "standard")
-*/
     streams.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
       override def uncaughtException(t: Thread, e: Throwable): Unit = {
         System.out.println("Uncaught exception on thread " + t + " " + e.toString)
@@ -64,9 +80,41 @@ object KafkaModelServer {
       try {
         streams.close()
       } catch {
-        case e: Exception =>
-        // ignored
+        case NonFatal(e) => println(s"During streams.close(), received: $e")
       }
     }
+  }
+
+  /*
+   * Setup streams with the built-in ("standard") store.
+   * Create the topology and the rest service.
+   */
+  private def setupStandardStoreStreams(streamsConfiguration: Properties): KafkaStreams = {
+    val streams = StandardStoreStreamBuilder.createStreamsFluent(streamsConfiguration)
+    // Start the Restful proxy for servicing remote access to state stores
+    RestServiceStore.startRestProxy(streams, port, "standard")
+    streams
+  }
+
+  /*
+   * Setup streams with a custom store.
+   * Create the topology and the rest service.
+   */
+  private def setupCustomStoreStreams(streamsConfiguration: Properties): KafkaStreams = {
+    val streams = CustomStoreStreamBuilder.createStreamsFluent(streamsConfiguration)
+    // Start the Restful proxy for servicing remote access to state stores
+    RestServiceStore.startRestProxy(streams, port, "custom")
+    streams
+  }
+
+  /*
+   * Setup streams with an in-memory ("naive") store.
+   * Create the topology and the rest service.
+   */
+  private def setupMemoryStoreStreams(streamsConfiguration: Properties): KafkaStreams = {
+    val streams = MemoryStoreStreamBuilder.createStreams(streamsConfiguration)
+    // Start the Restful proxy for servicing remote access to state stores
+    RestServiceInMemory.startRestProxy(streams, port)
+    streams
   }
 }
