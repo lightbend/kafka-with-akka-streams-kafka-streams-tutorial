3f78f557883f0d3a1f30c280e89596e1e1c2e388
Dean Wampler <dean@concurrentthought.com>
2018-08-10 19:40:26 -0500

    Version consistency

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,7 +2,7 @@
   * Created by boris on 7/14/17.
   */
 object Versions {
-  val Scala             = "2.11.11"
+  val Scala             = "2.11.12"
   val JDK               = "1.8"
 
   val reactiveKafkaVersion = "0.21.1"
